c12d4457b6e594eff81e49b17b6382aee8653631
Alaa <mir.alaa96@gmail.com>
2020-05-09 17:04:46 -0400

    fix core issue

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var router = require('./routers/router')
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
